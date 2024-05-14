c0218488bf2df16038c707fb460d29aaa7cc5c71
Ealam <ealozaher@gmail.com>
2024-05-14 20:54:57 +0300

    almost completed the navBar

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,5 +1,24 @@
 const header = () => {
-  return <div>header</div>;
+  const navLinks = ["About", "Articles", "Projects", "Speaking", "Contact"];
+  return (
+    <header className="flex flex-row items-center justify-between px-10  ">
+      <nav className=" py-3 px-6 bg-header rounded-[55px]">
+        <ul className="flex flex-row gap-4">
+          {navLinks.map((item, key) => (
+            <li key={key}>
+              <a
+                href=""
+                className=" text-title opacity-90 text-sm font-medium hover:opacity-100 hover:text-[0.92rem] hover:text-specialBlue duration-200"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button className=" text-xl">light</button>
+    </header>
+  );
 };
 
 export default header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ export default {
       colors:{
         primary : "#000",
         secondary : "rgb(24,24,27)",
-        borderPrimary : "rgba(63, 63, 70, 0.4)"
+        borderPrimary : "rgba(63, 63, 70, 0.4)",
+        title: "rgb(244,244,245)",
+        subtitle : "rgba(161,161,170)",
+        header : 'rgb(39,39,42)',
+        specialBlue: "rgb(93,188,252)"
       }
     },
   },
