ebfd1c11935c4999be679bb1f1e71ccbf9b9b727
Ealam <ealozaher@gmail.com>
2024-05-14 20:04:02 +0300

    Coded the base structure of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import Header from "./components/1-header/Header";
+import Hero from "./components/2-hero/Hero";
+import Main from "./components/3-main/Main";
+import Contact from "./components/4-contact/Contact";
+import Footer from "./components/5-footer/Footer";
+
+function App() {
+  const hrStyle = "my-10 border border-borderPrimary";
+  return (
+    <div className=" m-auto w-[80%]  border-2 border-borderPrimary  bg-secondary text-white">
+      <Header />
+      <hr className={`${hrStyle}`} />
+      <Hero />
+      <hr className={`${hrStyle}`} />
+      <Main />
+      <hr className={`${hrStyle}`} />
+      <Contact />
+      <hr className={`${hrStyle}`} />
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
-      
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"]
+        
+      },
+      colors:{
+        primary : "#000",
+        secondary : "rgb(24,24,27)",
+        borderPrimary : "rgba(63, 63, 70, 0.4)"
+      }
     },
   },
   plugins: [],
