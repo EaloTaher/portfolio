3060d3d9f7b01c054022720312125156048c3463
Ealam <ealozaher@gmail.com>
2024-05-15 18:28:37 +0300

    Completed the header

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -7,13 +7,12 @@ const Header = () => {
     <header className="flex flex-row items-center justify-between px-10 mt-5  ">
       {/* In Case of the screens that are less than 768px the hamburger menu will be unhidden and the nav will be hidden */}
       <button
-        className=" text-xl hidden max-md:block "
+        className=" text-2xl size-10 hidden max-md:block icon-menu bg-header rounded-full hover:text-title  text-subtitle hover:border border-[rgba(244,165,96,0.249)] duration-200 active:scale-90  "
         onClick={() => {
           setShowModal(true);
         }}
-      >
-        Show Modal
-      </button>
+      />
+
       <div />
       <nav className=" py-3 px-6 bg-header rounded-[55px] shadow-[1px_1px_40px_#2323242a]  max-md:hidden">
         <ul className="flex flex-row gap-4">
@@ -21,7 +20,7 @@ const Header = () => {
             <li key={key}>
               <a
                 href=""
-                className=" text-title opacity-90 text-sm font-medium hover:opacity-100 hover:text-[0.92rem] hover:text-specialBlue duration-200"
+                className=" text-title opacity-90 text-sm font-medium hover:opacity-100 hover:text-[0.92rem] hover:text-specialBlue duration-200 "
               >
                 {item}
               </a>
@@ -29,19 +28,20 @@ const Header = () => {
           ))}
         </ul>
       </nav>
-      <button className=" text-xl">light</button>
+      <button className=" size-10  bg-header  rounded-full hover:border text-subtitle hover:text-title border-[rgba(244,165,96,0.249)] duration-200 active:scale-90">
+        <span className="icon-moon  text-2xl     " />
+      </button>
       {showModal && (
-        <div className="fixed inset-0  bg-[rgb(40,40,48)] opacity-90">
-          <div className="w-[85%] m-auto mt-8 rounded-2xl px-4 pt-8 pb-0 bg-secondary  animate-popup">
+        <div className="fixed inset-0  bg-[rgb(40,40,48)] opacity-95">
+          <div className="w-[85%] m-auto mt-8 rounded-2xl px-6 pt-4 pb-0 bg-secondary  animate-popup">
             <ul>
-              <li className="text-right">
+              <li className="text-right -mb-4">
                 <button
+                  className="icon-close  text-2xl text-subtitle hover:text-3xl hover:text-red-600 duration-200 hover:rotate-180  "
                   onClick={() => {
                     setShowModal(false);
                   }}
-                >
-                  close
-                </button>
+                />
               </li>
               {navLinks.map((item, key) => (
                 <li
