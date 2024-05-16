38017cdb2c0b7b2ac1ce7c60bdfdbb3d57d08429
Ealam <ealozaher@gmail.com>
2024-05-16 21:10:45 +0300

    completed the main section

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -2,13 +2,49 @@ const Main = () => {
   const leftButtons = ["all projects", "React & Tailwind", "Node & Express"];
 
   return (
-    <main className="flex gap-12 ">
-      <section className="border border-red-500 flex flex-col gap-3">
+    <main className="flex gap-12 items-start max-[700px]:flex-col max-[700px]:items-center ">
+      <section className=" flex flex-col gap-3 max-[700px]:flex-row max-[700px]:flex-wrap max-[700px]:justify-center">
         {leftButtons.map((btn, key) => (
-          <button key={key}>{btn}</button>
+          <button
+            key={key}
+            className=" w-44 px-3 py-2 bg-[rgba(36,37,46,1)] text-lg capitalize opacity-50 border first:border first:border-specialBlue
+             first:opacity-100 first:font-semibold  first: tracking-[0.6px] first:py-3 rounded hover:opacity-100 duration-300 
+              max-[700px]:px-1 max-[700px]:text-base max-[700px]:w-36"
+          >
+            {btn}
+          </button>
         ))}
       </section>
-      <section className="border border-red-500 grow"> right</section>
+      <section className=" border border-red-600 flex flex-wrap gap-x-4 gap-y-8 justify-center">
+        {["aa", "bb", "cc", "dd"].map((item, key) => (
+          <article
+            key={key}
+            className="border-[rgba(93,188,252,0.3)] border-[3px] rounded hover:border-specialBlue duration-300 hover:rotate-1 hover:scale-[1.02] hover:cursor-pointer bg-gradient-to-t from-[rgba(255,255,255,0.05)] to bg-[rgba(255,255,255,0.05)]"
+          >
+            <img src="./landing1.png" className=" w-72 rounded" />
+            <div className="w-72  py-3 px-2">
+              <h1 className="text-title font-bold ">Landing Page 1</h1>
+              <p className="text-subtitle text-sm mt-2 mb-5">
+                f type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic
+              </p>
+              <div className="flex justify-between">
+                <div className="flex gap-3 text-xl text-subtitle">
+                  <div className="icon-link hover:text-[1.5rem]  hover:text-iconHover" />
+                  <div className="icon-github hover:text-[1.5rem]  hover:text-iconHover" />
+                </div>
+                <a
+                  href=""
+                  className="text-specialBlue flex items-center gap-1 mr-1"
+                >
+                  more <span className="icon-arrow-right " />
+                </a>
+              </div>
+            </div>
+          </article>
+        ))}
+      </section>
     </main>
   );
 };
